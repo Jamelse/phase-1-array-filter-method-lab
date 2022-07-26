dc8c244e1be618465f783ffc93bd3a9c0ed54ee4
Jacob <jacobamelse@hotmail.com>
2022-07-25 20:15:42 -0500

    commit tree c84730475e87edf332f583ac5f4099b007c66608

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(driver, string){
+  return driver.filter(function(ja){
+    return ja.toLowerCase().indexOf(string.toLowerCase())!== -1
+  });
+}
+function fuzzyMatch(driver, string){
+  return driver.filter(function (ja){
+    return ja.startsWith(string);
+  })
+}
+let driver = [
+  {
+    name: 'Bobby',
+    hometown: 'Pittsburgh' },
+  {
+    name: 'Sammy',
+    hometown: 'New York' } ,
+  {
+    name: 'Sally',
+    hometown: 'Cleveland' },
+  {
+    name: 'Annette',
+    hometown: 'Los Angeles' },
+  {
+    name: 'Bobby',
+    hometown: 'Tampa Bay' }
+];
+function matchName(array, string){
+  return array.filter(function(ja){
+  return ja.name.toLowerCase() === string.toLowerCase()
+});
+  
+
+}
